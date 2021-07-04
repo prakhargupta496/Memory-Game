1819b6e899891b7512c7d2438033d4fde8fef7a7
Prakhar Gupta <54628037+prakhargupta496@users.noreply.github.com>
2021-07-05 00:14:21 +0530

    Completed the game

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const grid = document.querySelector(".grid");
+var chosenImg = [];
+var chosenImgId = [];
+var ans = 0;
 const imgArr = [
 {
     name: "crash",
@@ -48,12 +51,61 @@ imgArr.sort(() => Math.random() - 0.5);
 function createGrid(){
     for(let i=0;i<imgArr.length;i++){
         const cell = document.createElement("img");
-        cell.setAttribute('src',imgArr[i].src);
+        cell.setAttribute('src','./images/cover.jpg');
+        cell.setAttribute('id',i);
         cell.style.width = "60px";
         cell.style.height = "60px";
-        //cell.addEventListener("click",);
+        cell.addEventListener("click",flipImg);
         grid.appendChild(cell);
     }
+}
+
+
+
+
+function flipImg(){
+    const id = this.getAttribute('id');
+    this.setAttribute('src',imgArr[id].src);
+    chosenImg.push(imgArr[id].name);
+    chosenImgId.push(id);
+    if(chosenImg.length === 2){
+        setTimeout(function(){
+            if(chosenImgId[0] === chosenImgId[1]){
+                alert("Select different images !!");
+                const cell = document.querySelectorAll("img");
+                cell[chosenImgId[0]].setAttribute('src','./images/cover.jpg');
+                cell[chosenImgId[1]].setAttribute('src','./images/cover.jpg');
+            }else if(chosenImg[0] === chosenImg[1]){
+                ans++;
+                const cell = document.querySelectorAll("img");
+                
+                console.log(cell[chosenImgId[0]]);
+                
+                cell[chosenImgId[0]].setAttribute('src','./images/white.png');
+                cell[chosenImgId[1]].setAttribute('src','./images/white.png');
+                cell[chosenImgId[0]].removeEventListener("click",flipImg);
+                cell[chosenImgId[1]].removeEventListener("click",flipImg);
+                chosenImg = [];
+                chosenImgId = [];
+                
+            }else{
+                const cell = document.querySelectorAll("img");
+                console.log(cell[chosenImgId[0]]);
+                cell[chosenImgId[0]].setAttribute('src','./images/cover.jpg');
+                cell[chosenImgId[1]].setAttribute('src','./images/cover.jpg');
+                alert("wrong images!!");
+                chosenImg = [];
+                chosenImgId = [];
+            }
+            
+            document.querySelector(".score").innerHTML = ans;
+            if(ans === imgArr.length/2){
+                
+                document.querySelector(".score").innerHTML = "Congrats, you have won";
+            }
+        },100);
+    }
+    
 }
 
 createGrid();
