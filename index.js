047af05f694d1a50b527fe898a5d4259d222f555
Prakhar Gupta <54628037+prakhargupta496@users.noreply.github.com>
2021-07-04 21:52:34 +0530

    Made the grid and added random images

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
 const grid = document.querySelector(".grid");
+const imgArr = [
+{
+    name: "crash",
+    src: "./images/crash.png"
+},
+{
+    name: "kick",
+    src: "./images/kick.png"
+},
+{
+    name: "snare",
+    src: "./images/snare.png"
+},
+{
+    name: "tom1",
+    src: "./images/tom1.png"
+},
+{
+    name: "tom2",
+    src: "./images/tom2.png"
+},
+{
+    name: "crash",
+    src: "./images/crash.png"
+},
+{
+    name: "kick",
+    src: "./images/kick.png"
+},
+{
+    name: "snare",
+    src: "./images/snare.png"
+},
+{
+    name: "tom1",
+    src: "./images/tom1.png"
+},
+{
+    name: "tom2",
+    src: "./images/tom2.png"
+}];
+
+imgArr.sort(() => Math.random() - 0.5);
+
+
 
 function createGrid(){
-    for(let i=0;i<20;i++){
+    for(let i=0;i<imgArr.length;i++){
         const cell = document.createElement("img");
-        cell.setAttribute('src','./images/crash.png');
+        cell.setAttribute('src',imgArr[i].src);
         cell.style.width = "60px";
         cell.style.height = "60px";
-        grid.appendChild(cell);
-    }
-    for(let i=0;i<20;i++){
-        const cell = document.createElement("img");
-        cell.setAttribute('src','./images/kick.png');
-        cell.style.width = "60px";
-        cell.style.height = "60px";
+        //cell.addEventListener("click",);
         grid.appendChild(cell);
     }
 }
